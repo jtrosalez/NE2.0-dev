bb7e78c6e4d6f769f73fa1c82a54341d87ce00fb
jtrosalez <jtrosalez@gmail.com>
2024-07-01 15:10:09 -0700

    Update loadMap.js

--- js/loadMap.js
+++ js/loadMap.js
@@ -195,6 +195,7 @@ function initializeMap() {
 		$.ajax({
 			url: url17,
 			dataType: 'jsonp',
+			async: false,
 			success: function(response) {
 				//console.log(response.result.addressMatches[0].geographies); //This will allow you to view all the results that you get for the address geocoder
 				var tract17v1 = (response.result.addressMatches[0].geographies['Census Tracts'][0]['NAME']); //Remove the word Census from the tract name to match the AskCHIS Census Tract naming convention
